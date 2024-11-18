73c3c61cc031fee7654978d4008437a4f367a8a1
pavelcium23 <148584451+pavelcium23@users.noreply.github.com>
2024-11-18 02:27:38 +0200

    Task 4 Bonus Task(end of lab 1)

--- lab-papers-please/java-classifcation/src/main/java/oop/practice/Main.java
+++ lab-papers-please/java-classifcation/src/main/java/oop/practice/Main.java
@@ -1,9 +1,11 @@
 package oop.practice;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Display display1 = new Display(3840, 2160, 163, "Samsung Odyssey G9");
         Display display2 = new Display(1920, 1080, 82, "HP Pavilion 22cwa");
         Display display3 = new Display(3440, 1440, 110, "Acer Predator X34");
+
 
         System.out.println("\nTask 1 the comparison of displays:");
         display1.compareSize(display2);
@@ -12,6 +14,7 @@ public class Main {
         display2.compareSharpness(display3);
         display1.compareWithMonitor(display3);
 
+        System.out.println("\nTask 3 the assistant:");
         Assistant assistant = new Assistant("Assisant");
         assistant.assignDisplay(display1);
         assistant.assignDisplay(display2);
@@ -19,24 +22,41 @@ public class Main {
 
         assistant.assist();
 
-
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        scanner.nextLine();
 //reading the files
-        String filePath = "/Users/suleimanpasa/oop-course-repo/lab-papers-please/java-classifcation/src/main/resources/text.txt";
+        System.out.println("\nTask 4(updated task 2) the file reader:");
+        String filePaths[] = {"/Users/suleimanpasa/oop-course-repo/lab-papers-please/java-classifcation/src/main/resources/text.txt",
+                "/Users/suleimanpasa/oop-course-repo/lab-papers-please/java-classifcation/src/main/resources/text2.txt",
+                "/Users/suleimanpasa/oop-course-repo/lab-papers-please/java-classifcation/src/main/resources/text3.txt"};
         FileReader fileReader = new FileReader();
-        String fileContent = fileReader.readFileIntoString(filePath);
+        System.out.println("Choose a file to read:");
+        for (int i = 0; i < filePaths.length; i++) {
+            System.out.println((i + 1) + ". " + filePaths[i]);
+        }
+        System.out.println("Enter the number of the file you want to read:");
+        int fileChoice = scanner.nextInt() - 1;
 
-        // creating TextData
-        TextData textData = new TextData(filePath, fileContent);
+        if (fileChoice >= 0 && fileChoice < filePaths.length) {
+            System.out.println("File to check: " + filePaths[fileChoice]);
+            String fileContent = fileReader.readFileIntoString(filePaths[fileChoice]);
+            TextData textData = new TextData(filePaths[fileChoice], fileContent);
 
-        System.out.println("File name: " + textData.getFileName());
-        System.out.println("File content: " + textData.getText());
-        System.out.println("Number of vowels: " + textData.getNumberOfVowels());
-        System.out.println("Number of consonants: " + textData.getNumberOfConsonants());
-        System.out.println("Number of letters: " + textData.getNumberOfLetters());
-        System.out.println("Number of sentences: " + textData.getNumberOfSentences());
-        System.out.println("The longest word: " + textData.getLongestWord());
+            System.out.println("File name: " + textData.getFileName());
+            System.out.println("File content: " + textData.getText());
+            System.out.println("Number of vowels: " + textData.getNumberOfVowels());
+            System.out.println("Number of consonants: " + textData.getNumberOfConsonants());
+            System.out.println("Number of letters: " + textData.getNumberOfLetters());
+            System.out.println("Number of sentences: " + textData.getNumberOfSentences());
+            System.out.println("The longest word: " + textData.getLongestWord());
+        } else {
+            System.out.println("Invalid input. Please try again.");
+        }
+        scanner.close();
+
     }
-
-
 }
+
+
 
